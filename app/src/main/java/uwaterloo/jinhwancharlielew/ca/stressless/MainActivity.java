5bd78d0ba077216861ef7993b3f0d572c7b0b1d6
Jinhwan Lew <j3lew@edu.uwaterloo.ca>
2019-01-12 20:34:06 -0500

    Button for next page

--- app/src/main/java/uwaterloo/jinhwancharlielew/ca/stressless/MainActivity.java
+++ app/src/main/java/uwaterloo/jinhwancharlielew/ca/stressless/MainActivity.java
@@ -1,7 +1,11 @@
 package uwaterloo.jinhwancharlielew.ca.stressless;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                openActivity2();
+            }
+        });
     }
+    public void openActivity2() {
+        Intent secondPageIntent = new Intent(this, Main2Activity.class);
+        startActivity(secondPageIntent);
+    }
+
 }
